70a4bb2739770ca0c20c7eb2a634652243afc35c
Denis <denis.lessin@gmail.com>
2017-07-29 19:23:26 +0200

    scrobble test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
 	function getParams () {
 		var hashParams = {};
 		var e, r = /([^&;=]+)=?([^&;]*)/g;
@@ -14,11 +15,20 @@ $(document).ready(function () {
 	var $getTextButton = $('#get_text_button');
 	var $loginButton = $('#login-button');
 
+	var lastFmUrl = 'https://ws.audioscrobbler.com/2.0/';
 	var apiKey = '85255598eb489a85d75ef556169fd824';
 	var secret = '19c8cc5704536b2a8e6805b2a62f7e2e';
 
+	var SESSION_KEY_STORAGE_NAME = 'sessionKey';
+	var sessionKey = localStorage.getItem(SESSION_KEY_STORAGE_NAME);
+
 	var params = getParams();
 	var token = params.token;
+
+	if (sessionKey == null) {
+		$loginButton.show();
+	}
+
 
 	if (token != null) {
 		$loginButton.hide();
@@ -28,8 +38,6 @@ $(document).ready(function () {
 		var sigTokenStr = 'token' + token;
 		var sigStr = sigApiKeyStr + sigMethodStr + sigTokenStr + secret;
 		var sigHash = hex_md5(sigStr);
-
-		console.log(sigHash);
 
 		var getSessionArgs = {
 			method: 'auth.getSession',
@@ -41,10 +49,13 @@ $(document).ready(function () {
 
 		$.ajax({
 			type: 'GET',
-			url: 'https://ws.audioscrobbler.com/2.0/',
+			url: lastFmUrl,
 			data: getSessionArgs,
 			success: function (data) {
 				console.log('success', data);
+
+				sessionKey = data.key;
+				localStorage.setItem(SESSION_KEY_STORAGE_NAME, sessionKey);
 			},
 			error: function (code, message) {
 				console.log(code, message);
@@ -54,35 +65,69 @@ $(document).ready(function () {
 		$loginButton.show();
 	}
 
-    $loginButton.click(function () {
-			var url = 'http://www.last.fm/api/auth/?api_key=' + apiKey;
+	$loginButton.click(function () {
+		var url = 'http://www.last.fm/api/auth/?api_key=' + apiKey;
+		window.location.href = url;
+	});
 
-			window.location.href = url;
-		});
+	$getTextButton.click(function () {
+		var text = $textbox.val();
+		var lines = text.split('\n');
 
-	    $getTextButton.click(function () {
-				var text = $textbox.val();
-				var lines = text.split('\n');
+		for (var i = 0; i < lines.length; i++) {
+			if (/-/.test(lines[i])) {
+				var artistAndTitle = lines[i].split('-', 2);
+				var artist = artistAndTitle[0];
+				var title = artistAndTitle[1];
+				artist = artist.match(/[^\d\s.].*[^\s]/)[0];
 
-        for (var i = 0; i < lines.length; i++) {
-            if (/-/.test(lines[i])) {
-                var artistAndTitle = lines[i].split('-', 2);
-                var artist = artistAndTitle[0];
-                var title = artistAndTitle[1];
-                artist = artist.match(/[^\d\s.].*[^\s]/)[0];
+				if (/\[/.test(title)) {
+					title = title.match(/(?!\s).*(?=\s\[.*)/)[0];
+				} else {
+					title = title.match(/(?!\s).*/)[0];
+				}
 
-                if (/\[/.test(title))
-                    title = title.match(/(?!\s).*(?=\s\[.*)/)[0];
-                else
-                    title = title.match(/(?!\s).*/)[0];
+				console.log('Artist: ' + artist, 'Title: ' + title);
+			}
+		}
 
-                console.log('Artist: ' + artist, 'Title: ' + title);
-            }
-        }
+		var sigApiKeyStr = 'api_key' + apiKey;
+		var sigArtistStr = 'artist' + 'Implex';
+		var sigMethodStr = 'method' + 'track.scrobble';
+		var sigTokenStr = 'token' + token;
+		var sigTrackStr = 'track' + 'You';
+
+		var sigStr = sigApiKeyStr + sigArtistStr + sigMethodStr + sigTokenStr + sigTrackStr + secret;
+		var sigHash = hex_md5(sigStr);
+
+		var scrobbleArgs = {
+			method: 'track.scrobble',
+			artist: 'Implex',
+			track: 'You',
+			api_key: apiKey,
+			token: token,
+			api_sig: sigHash,
+			format: 'json'
+		};
 
 		$.ajax({
 			type: 'POST',
-			url: 'https://ws.audioscrobbler.com/2.0/',
+			url: lastFmUrl,
+			data: scrobbleArgs,
+			success: function (data) {
+				console.log('success', data);
+
+				sessionKey = data.key;
+				localStorage.setItem(SESSION_KEY_STORAGE_NAME, sessionKey);
+			},
+			error: function (code, message) {
+				console.log(code, message);
+			}
+		});
+
+		$.ajax({
+			type: 'POST',
+			url: lastFmUrl,
 			data: 'method=artist.getinfo&' +
 				   'artist=Burial&' +
 				   'api_key=85255598eb489a85d75ef556169fd824&' +
