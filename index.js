d739a4e315454c88febc43de083adf49ac7fce67
the-vein <30551473+the-vein@users.noreply.github.com>
2017-07-29 15:47:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,34 @@ $(document).ready(function(){
     var $textbox = $("textarea");    
     var $getTextButton = $("#get_text_button");
     
+    $("#login-button").click(function() {
+		// localStorage.removeItem(ACCESS_TOKEN_KEY);
+		// var state = generateRandomString(16);
+		// localStorage.setItem(STATE_KEY, state);
+		// var dateLogin = Math.floor(Date.now() / 1000);
+		// localStorage.setItem(DATE_LOGIN_KEY, dateLogin);
+
+		// var client_id = "0021f16415934279a9f094535452a760";
+		// var scope = "playlist-read-private";
+
+		// var redirect_uri;
+		// var localHost = "localhost:8005";
+		// var localFromNetwork = "192.168.178.40:8005";
+		// if (location.host === localHost) {
+		// redirect_uri = "http://" + localHost;
+		// }
+		// else if (location.host === localFromNetwork) {
+		// redirect_uri = "http://" + localFromNetwork;
+		// }
+		// else {
+		// redirect_uri = "https://5myg.github.io";
+		// }
+
+		var url = "http://www.last.fm/api/auth/?api_key=85255598eb489a85d75ef556169fd824";
+
+		window.location.href = url;
+    });	
+	
     $getTextButton.click(function() {
         var text = $textbox.val();
         var lines = text.split("\n");
