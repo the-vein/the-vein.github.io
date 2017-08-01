7de8ca0d124437f6fd554a09817345a9c99ad0fd
Denis <denis.lessin@gmail.com>
2017-08-01 22:10:05 +0200

    fix params

--- index.js
+++ index.js
@@ -85,13 +85,10 @@ $(document).ready(function () {
 			return;
 		}
 
-		var unixTimeStamp = Math.round(Date.now() / 1000) - 10800;  //TODO: fix correct time
+		var unixTimeStamp = Math.round(Date.now() / 1000) - 10800;  // TODO: fix correct time
 		var sigArtistStr = '';
 		var sigTrackStr = '';
 		var sigTimeStampStr = '';
-		var paramArtist = '';
-		var paramTrack = '';
-		var paramTimeStamp = '';
 		var scrobbles = [];
 		var indices = [];
 
@@ -116,46 +113,72 @@ $(document).ready(function () {
 					timestamp: unixTimeStamp
 				});
 
+				// TODO: show scrobbles to user for confirmation / removeable
+
+				if (scrobbles.length > 50) {
+					alert('lastfm doesnt allow more than 50 scrobbles in a bulk at once');
+					return;
+				}
+
 				indices.push(i);
 			}
 		}
 
 		indices.sort(lastFmParamsSorting);
 
+		var paramData = {};
+
 		for (var j = 0; j < indices.length; j++) {
 			var index = indices[j];
 			var scrobble = scrobbles[index];
 
-			sigArtistStr += 'artist[' + index + ']' + scrobble.artist;
-			paramArtist += '&artist[' + index + ']=' + scrobble.artist;
-			sigTrackStr += 'track[' + index + ']' + scrobble.track;
-			paramTrack += '&track[' + index + ']=' + scrobble.track;
-			sigTimeStampStr += 'timestamp[' + index + ']' + scrobble.timestamp;
-			paramTimeStamp += '&timestamp[' + index + ']=' + scrobble.timestamp;
+			var artistParam = 'artist[' + index + ']';
+			sigArtistStr += artistParam + scrobble.artist;
+			paramData[artistParam] = scrobble.artist;
+
+			var trackParam = 'track[' + index + ']';
+			sigTrackStr += trackParam + scrobble.track;
+			paramData[trackParam] = scrobble.track;
+
+			var timestampParam = 'timestamp[' + index + ']';
+			sigTimeStampStr += timestampParam + scrobble.timestamp;
+			paramData[timestampParam] = scrobble.timestamp;
 		}
 
-		var sigApiKeyStr = 'api_key' + apiKey;
-		var sigMethodStr = 'method' + 'track.scrobble';
-		var sigSessionKeyStr = 'sk' + sessionKey;
+		var apiKeyParam = 'api_key';
+		var methodParam = 'method';
+		var method = 'track.scrobble';
+		var sessionKeyParam = 'sk';
+
+		var sigApiKeyStr = apiKeyParam + apiKey;
+		var sigMethodStr = methodParam + method;
+		var sigSessionKeyStr = sessionKeyParam + sessionKey;
 		var sigStr = sigApiKeyStr + sigArtistStr + sigMethodStr +
 			sigSessionKeyStr + sigTimeStampStr + sigTrackStr + secret;
 
 		var sigHash = hex_md5(sigStr);
 
+		paramData[apiKeyParam] = apiKey;
+		paramData[methodParam] = method;
+		paramData[sessionKeyParam] = sessionKey;
+		paramData.api_sig = sigHash;
+		paramData.format = 'json';
+
 		console.log(sigArtistStr);
-		console.log(paramArtist);
+		console.log(sigTrackStr);
+		console.log(paramData);
 
 		$.ajax({
 			type: 'POST',
-			url: lastFmUrl + '?api_key=' + apiKey + '&api_sig=' + sigHash + paramArtist +
-				'&method=track.scrobble&sk=' + sessionKey +
-				paramTimeStamp + paramTrack + '&format=json',
+			url: lastFmUrl,
+			data: paramData,
 			success: function (data) {
 				console.log('success', data);
 				alert("success scrobbling");
 			},
 			error: function (response) {
 				console.log(response.responseText);
+				alert("something went wrong");
 			}
 		});
 
